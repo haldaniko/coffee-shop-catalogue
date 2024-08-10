342eee093700c4f521f7eff4cf4f6358dad2e4c6
haldaniko <haldaniko@gmail.com>
2024-08-10 04:59:38 +0200

    create CRUD viewsets for all models

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Tag,
+    Socials,
+    Address,
+    WorkTime,
+    CoffeeShop,
+    GalleryImage,
+    Comment,
+    Review
+)
+from .serializers import (
+    TagSerializer,
+    SocialsSerializer,
+    AddressSerializer,
+    WorkTimeSerializer,
+    CoffeeShopSerializer,
+    GalleryImageSerializer,
+    CommentSerializer,
+    ReviewSerializer
+)
 
-# Create your views here.
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class SocialsViewSet(viewsets.ModelViewSet):
+    queryset = Socials.objects.all()
+    serializer_class = SocialsSerializer
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class WorkTimeViewSet(viewsets.ModelViewSet):
+    queryset = WorkTime.objects.all()
+    serializer_class = WorkTimeSerializer
+
+
+class CoffeeShopViewSet(viewsets.ModelViewSet):
+    queryset = CoffeeShop.objects.all()
+    serializer_class = CoffeeShopSerializer
+
+
+class GalleryImageViewSet(viewsets.ModelViewSet):
+    queryset = GalleryImage.objects.all()
+    serializer_class = GalleryImageSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
