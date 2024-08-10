1779a405a2c7c3cf0ba868a17fda2affa87a0bb7
haldaniko <haldaniko@gmail.com>
2024-08-10 04:50:44 +0200

    register model 'User' in admin.py

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from user.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ('username', 'email', 'is_owner', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'is_owner')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'photo', 'is_owner')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions', 'groups')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'photo', 'is_owner'),
+        }),
+    )
