2d3289ece27112e4af3d4d2e61b7197c041a6b4e
haldaniko <haldaniko@gmail.com>
2024-08-22 16:25:10 +0200

    implement auth logic

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,32 +1,63 @@
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = '__all__'
+        model = get_user_model()
+        fields = ("id",
+                  "username",
+                  "email",
+                  "password",
+                  "is_owner",
+                  "is_staff",
+                  "first_name",
+                  "last_name",
+                  "photo")
+        read_only_fields = ("is_staff", "is_owner")
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            email=validated_data.get('email', ''),
-            **validated_data
-        )
-        return user
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.username = validated_data.get('username', instance.username)
-        instance.email = validated_data.get('email', instance.email)
-        instance.is_owner = validated_data.get('is_owner', instance.is_owner)
-        instance.photo = validated_data.get('photo', instance.photo)
-
-        password = validated_data.get('password')
+        """Update a user, set the password correctly and return it"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
         if password:
-            instance.set_password(password)
+            user.set_password(password)
+            user.save()
 
-        instance.save()
-        return instance
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(label=_("Email"))
+    password = serializers.CharField(
+        label=_("Password"), style={"input_type": "password"}
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(email=email, password=password)
+
+            if user:
+                if not user.is_active:
+                    msg = _("User account is disabled.")
+                    raise serializers.ValidationError(
+                        msg,
+                        code="authorization")
+            else:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = _("Must include 'username' and 'password'.")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
