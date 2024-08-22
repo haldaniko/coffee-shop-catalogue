2ca6f4fb3940fbefa5c7d8724fd148adece1f709
haldaniko <haldaniko@gmail.com>
2024-08-22 15:42:45 +0200

    register 'user.User' as AUTH_USER_MODEL

--- backend/catalog/settings.py
+++ backend/catalog/settings.py
@@ -98,3 +98,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "user.User"
