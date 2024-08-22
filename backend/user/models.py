b96032c6018cb8aec6a169086c837ff6af6ed5e3
haldaniko <haldaniko@gmail.com>
2024-08-22 16:24:24 +0200

    add 'image_file_path' func for 'User' model

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,9 +1,20 @@
+import os
+import uuid
+
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+from django.utils.text import slugify
+
+
+def image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.first_name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join(f"uploads/profile_img/", filename)
 
 
 class User(AbstractUser):
-    photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    photo = models.ImageField(upload_to=image_file_path, null=True, blank=True)
     is_owner = models.BooleanField(default=False)
     favorite_shops = models.ManyToManyField('website.CoffeeShop', related_name='favorited_by', blank=True)
 
