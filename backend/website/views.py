7947fc86c49104467a6d61c3efddf30be66eceb0
haldaniko <haldaniko@gmail.com>
2024-08-26 09:25:45 +0200

    implement filtering logic for CoffeeShopViewSet

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg, Q
 from rest_framework import viewsets
 from .models import (
     Tag,
@@ -43,6 +44,46 @@ class WorkTimeViewSet(viewsets.ModelViewSet):
 
 class CoffeeShopViewSet(viewsets.ModelViewSet):
     queryset = CoffeeShop.objects.all()
+    serializer_class = CoffeeShopSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        city = self.request.query_params.get("city")
+        name = self.request.query_params.get("name")
+        address = self.request.query_params.get("address")
+        with_owner = self.request.query_params.get("with_owner")
+        tags = self.request.query_params.get("tags")
+        min_rating = self.request.query_params.get("min_rating")
+        max_rating = self.request.query_params.get("max_rating")
+
+        if city:
+            queryset = queryset.filter(address__city__icontains=city)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if address:
+            queryset = queryset.filter(address__street__icontains=address)
+        if with_owner is not None:
+            has_owner = with_owner.lower() == "true"
+            queryset = queryset.filter(owner__isnull=not has_owner)
+        if tags:
+            tags_list = tags.split(',')
+            for i in tags_list:
+                queryset = queryset.filter(tags__name__icontains=i)
+
+        if min_rating is not None or max_rating is not None:
+            min_rating = int(min_rating) if min_rating is not None else None
+            max_rating = int(max_rating) if max_rating is not None else None
+            rating_filter = Q()
+            if min_rating is not None:
+                rating_filter &= Q(review__stars__gte=min_rating)
+            if max_rating is not None:
+                rating_filter &= Q(review__stars__lte=max_rating)
+            queryset = queryset.annotate(
+                average_rating=Avg('review__stars')
+            ).filter(rating_filter)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
