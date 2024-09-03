7619eead9964bdb1cc31def8b753caf5e02f9926
haldaniko <haldaniko@gmail.com>
2024-09-03 17:03:06 +0200

    add ChangePasswordView

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,9 +1,12 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.generics import UpdateAPIView
 from rest_framework.settings import api_settings
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from backend.user.serializers import ChangePasswordSerializer
+from user.models import User
 from user.serializers import UserSerializer, AuthTokenSerializer
 
 
@@ -23,3 +26,31 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class ChangePasswordView(UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
