7793a4c64da84cf8488ce6b6e7be7b2664bfc31a
haldaniko <haldaniko@gmail.com>
2024-09-19 20:10:27 +0200

    implement like/dislike logic for Reviews

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CoffeeShopViewSet, CityStatsListView, IndexCoffeeShopListView
+from .views import CoffeeShopViewSet, CityStatsListView, IndexCoffeeShopListView, LikeReview, DislikeReview
 
 router = DefaultRouter()
 router.register(r'coffeeshops', CoffeeShopViewSet)
@@ -9,7 +9,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('city-stats/', CityStatsListView.as_view(), name='city-stats-list'),
     path('index-coffee-shops/', IndexCoffeeShopListView.as_view(), name='coffee-shops-list'),
+    path('reviews/<int:pk>/like/', LikeReview.as_view(), name='like-review'),
+    path('reviews/<int:pk>/dislike/', DislikeReview.as_view(), name='dislike-review'),
 ]
-
 
 app_name = "website"
